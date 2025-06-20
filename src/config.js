791454bc0513fa86323ce2e53360c76416859895
ZilatAIOBC <146693454+ZilatAIOBC@users.noreply.github.com>
2025-06-21 04:44:51 +0500

    Update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /** @format */
 
 const config = {
-	BACKEND_URL: import.meta.env.VITE_BACKEND_URL || 'https://5cb1-2402-ad80-6b-223f-14e0-bac7-ce35-b8d2.ngrok-free.app',
+	BACKEND_URL: import.meta.env.VITE_BACKEND_URL || 'https://fc36-2402-ad80-6b-223f-14e0-bac7-ce35-b8d2.ngrok-free.app',
 };
 
 console.log('🔧 Frontend Config:', config);
